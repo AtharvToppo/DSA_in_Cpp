0391037ddf384886427f0e4851cf054f82fa47d6
Atharv <atharvtoppo1@gmail.com>
2026-08-21 17:22:26 +0530

    Time: 0 ms (100%), Space: 7.7 MB (49.14%) - LeetHub

---
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string defangIPaddr(string address)
+    {
+        string ans="";
+        int len=address.size();
+        for(int i=0;i<len;i++)
+        {
+            if(address[i]=='.')
+            {
+                ans+="[.]";
+            }
+            else
+            {
+                ans+=address[i];
+            }
+        }
+        return ans;
+    }
+};
